a80d993faf5b893cc88745040a324ae14b6f9f81
Soulaymane Boulaich <soulaiman.boulaich0@gmail.com>
2026-01-02 13:46:29 +0100

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://127.0.0.1:3001',
         changeOrigin: true,
         secure: false,
       }
